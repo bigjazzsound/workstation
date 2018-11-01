d89f73ba9100d6e06f9cd200088506cf49f3b6f3
bigjazzsound <craigjamesfielder@gmail.com>
2018-10-31 21:05:48 -0400

    added bus type to test

--- tests/build_test_vm.sh
+++ tests/build_test_vm.sh
@@ -1,15 +1,14 @@
 #!/bin/bash
 
-virsh destroy fedora28
-virsh undefine fedora28 --remove-all-storage
+virsh destroy fedora29
+virsh undefine fedora29 --remove-all-storage
 
 virt-install \
-    --name fedora28 \
+    --name fedora29 \
     --ram 8025 \
-    --disk path=/home/images/fedora28.qcow2,size=25 \
+    --disk path=/home/images/fedora29.qcow2,size=25,bus=virtio \
     --vcpus 6 \
     --os-type linux \
-    --os-variant fedora28 \
-    --location "$HOME/Downloads/Fedora-Everything-netinst-x86_64-28-1.1.iso" \
-    -x 'ks=http://192.168.122.1:8000/ks.cfg' \
-    # -x 'ks=https://raw.githubusercontent.com/bigjazzsound/workstation/master/ks.cfg'
+    --location "$HOME/Downloads/Fedora-Workstation-netinst-x86_64-29-1.2.iso" \
+    -x 'ks=https://raw.githubusercontent.com/bigjazzsound/workstation/master/ks.cfg'
+    # -x 'ks=http://192.168.122.1:8000/ks.cfg' \
