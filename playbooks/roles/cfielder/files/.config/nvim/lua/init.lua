0c4be803d77ec91fb00938fc0a71979c0beb69e0
Craig Fielder <craigjamesfielder@gmail.com>
2020-01-18 17:49:14 -0500

    Make lua config prettier and more succinct

--- playbooks/roles/cfielder/files/.config/nvim/lua/init.lua
+++ playbooks/roles/cfielder/files/.config/nvim/lua/init.lua
@@ -4,55 +4,50 @@ local DEFAULT_KEYMAP = { noremap = true, silent = true }
 local set_option = api.nvim_set_option
 local set_keymap = api.nvim_set_keymap
 
+-- disable unused providers
 vim.g.loaded_python_provider = 0
 vim.g.loaded_ruby_provider = 0
 vim.g.loaded_node_provider = 0
+
 vim.g.python3_host_prog = '/usr/bin/python3'
 
 api.nvim_win_set_option(0, 'number', true)
-set_option(
-  'statusline',
-  '[%n] %f%h%w%m%r  %{fugitive#head()} %{StatusDiagnostic()} %= %{&paste ?"PASTE ":""}%{&spell?"SPELL ":""} %{&ft}  %l/%L  %P '
-)
 
 api.nvim_command("command! Vimrc :edit $MYVIMRC")
 api.nvim_command("autocmd! VimResized * :wincmd =")
 
-set_option('syntax', 'on')
-set_option('termguicolors', true)
-set_option('inccommand', 'nosplit')
-set_option('splitbelow', true)
-set_option('splitright', true)
-set_option('cursorline', true)
-set_option('tags', 'tags')
-set_option('winblend', 5)
-set_option('laststatus', 2)
+options = {
+  syntax        = 'on',
+  termguicolors = true,
+  inccommand    = 'nosplit',
+  splitbelow    = true,
+  splitright    = true,
+  cursorline    = true,
+  tags          = 'tags',
+  winblend      = 5,
+  laststatus    = 2,
+  -- statusline    = '[%n] %f%h%w%m%r  %{fugitive#head()} %= %{&paste ?"PASTE ":""}%{&spell?"SPELL ":""} %{&ft}  %l/%L  %P ',
+  statusline    = '[%n] %f%h%w%m%r  %{fugitive#head()} %{StatusDiagnostic()} %= %{&paste ?"PASTE ":""}%{&spell?"SPELL ":""} %{&ft}  %l/%L  %P ',
+  -- settings for search
+  hlsearch      = true,
+  incsearch     = true,
+  -- settings for tabs
+  expandtab     = true,
+  autoindent    = true,
+  smarttab      = true,
+  listchars     = 'tab:..,trail:-,extends:>,precedes:<,nbsp:~',
+}
 
+for key, value in pairs(options) do set_option(key, value) end
+
+-- map ESC to jk
 set_keymap('i', 'jk', '<Esc>', DEFAULT_KEYMAP)
-set_keymap('i', '<Esc>', '', DEFAULT_KEYMAP)
 
 -- split navigation
 set_keymap('n', '<C-J>', '<C-W><C-J>', DEFAULT_KEYMAP)
 set_keymap('n', '<C-K>', '<C-W><C-K>', DEFAULT_KEYMAP)
 set_keymap('n', '<C-L>', '<C-W><C-L>', DEFAULT_KEYMAP)
 set_keymap('n', '<C-H>', '<C-W><C-H>', DEFAULT_KEYMAP)
-
--- settings for undofiles
-set_option('undofile', false)
-set_option('undodir', home .. '/.vim/undo/')
-set_option('backupdir', home .. '/.vim/backup/')
-set_option('directory', home .. '/.vim/swp/')
-
--- settings for search
-set_option('hlsearch', true)
-set_option('incsearch', true)
-
--- settings for tabs
-set_option('expandtab', true)
-set_option('autoindent', true)
-set_option('smarttab', true)
-
-set_option('listchars', 'tab:..,trail:-,extends:>,precedes:<,nbsp:~')
 
 -- 80 character color difference
 api.nvim_win_set_option(0, 'colorcolumn', '80')
@@ -71,42 +66,42 @@ set_keymap('c', '<A-b>', '<S-Left>', { noremap = true })
 set_keymap('c', '<A-e>', '<S-Right>', { noremap = true })
 
 -- Plugins
-local autoload_plug_path = api.nvim_eval("stdpath('config')") .. '/site/autoload/plug.vim'
+local autoload_plug_path = api.nvim_eval("stdpath('data')") .. '/site/autoload/plug.vim'
 local plug_file = io.open(autoload_plug_path , "r")
 if plug_file == nil then
-  os.execute('curl -fLo ' .. autoload_plug_path .. ' --create-dirs "https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim"')
-  api.nvim_command('autocmd VimEnter * PlugInstall --sync')
-  api.nvim_command('source $MYVIMRC')
+  os.execute('curl -fLso ' .. autoload_plug_path .. ' --create-dirs "https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim"')
+  os.exit()
 else
-  local plugins = {
-    "'christoomey/vim-tmux-navigator'",
-    "'editorconfig/editorconfig-vim'",
-    "'enricobacis/paste.vim'",
-    "'hashivim/vim-hashicorp-tools'",
-    "'herrbischoff/cobalt2.vim'",
-    "'iamcco/markdown-preview.nvim', { 'do': 'cd app & yarn install' }",
-    "'junegunn/fzf', { 'dir': '~/.fzf', 'do': './install --all' }",
-    "'junegunn/fzf.vim'",
-    "'junegunn/goyo.vim', { 'for': 'markdown' }",
-    "'junegunn/vim-easy-align'",
-    "'justinmk/vim-dirvish'",
-    "'justinmk/vim-sneak'",
-    "'mhinz/vim-signify'",
-    "'mhinz/vim-startify'",
-    "'neoclide/coc.nvim', { 'do': { -> coc#util#install() } }",
-    "'numirias/semshi', { 'do': ':UpdateRemotePlugins', 'for': 'python' }",
-    "'sheerun/vim-polyglot'",
-    "'tpope/vim-commentary'",
-    "'tpope/vim-dispatch'",
-    "'tpope/vim-eunuch'",
-    "'tpope/vim-fugitive'",
-    "'tpope/vim-obsession'",
-    "'tpope/vim-repeat'",
-    "'tpope/vim-surround'",
-  }
-  api.nvim_command("call plug#begin('" .. home .. "/.vim/plugged')")
-  for index, value in ipairs(plugins) do api.nvim_command("Plug " .. value) end
-  api.nvim_command("call plug#end()")
+  local plugins = [[
+'christoomey/vim-tmux-navigator'
+'editorconfig/editorconfig-vim'
+'enricobacis/paste.vim'
+'hashivim/vim-hashicorp-tools'
+'herrbischoff/cobalt2.vim'
+'iamcco/markdown-preview.nvim', { 'do': 'cd app & yarn install', 'for': 'markdown' }
+'junegunn/fzf', { 'dir': '~/.fzf', 'do': './install --all' }
+'junegunn/fzf.vim'
+'junegunn/goyo.vim', { 'for': 'markdown' }
+'junegunn/vim-easy-align'
+'justinmk/vim-dirvish'
+'justinmk/vim-sneak'
+'mhinz/vim-signify'
+'mhinz/vim-startify'
+'neoclide/coc.nvim', { 'do': { -> coc#util#install() } }
+'numirias/semshi', { 'do': ':UpdateRemotePlugins', 'for': 'python' }
+'tpope/vim-commentary'
+'tpope/vim-dispatch'
+'tpope/vim-eunuch'
+'tpope/vim-fugitive'
+'tpope/vim-obsession'
+'tpope/vim-repeat'
+'tpope/vim-surround'
+]]
+  vim.fn["plug#begin"](home .. "/.vim/plugged")
+  for _, value in ipairs(vim.split(vim.trim(plugins), "\n")) do
+    api.nvim_command("Plug " .. value)
+  end
+  vim.fn["plug#end"]()
 end
 
 api.nvim_command('colorscheme cobalt2')
@@ -135,7 +130,7 @@ set_keymap('n', '<leader>gps', ':Dispatch! git push<CR>', DEFAULT_KEYMAP)
 set_keymap('n', '<leader>gpl', ':Dispatch! git pull<CR>', DEFAULT_KEYMAP)
 
 -- editorconfig
-vim.g['EditorConfig_exclude_patterns'] = { 'fugitive://.\\*' }
+vim.g.EditorConfig_exclude_patterns = { 'fugitive://.\\*' }
 
 -- dispatch settings
 set_keymap('n', '<F3>', ':Dispatch<CR>', DEFAULT_KEYMAP)
@@ -189,4 +184,4 @@ function NavigationFloatingWin()
   end
 end
 
-vim.g['fzf_layout'] = { window = 'lua NavigationFloatingWin()' }
+vim.g.fzf_layout = { window = 'lua NavigationFloatingWin()' }
