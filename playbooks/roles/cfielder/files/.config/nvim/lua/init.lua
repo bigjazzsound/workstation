b9ef6000565ef0bc9874fdf8369182053f76d4e1
Craig Fielder <craigjamesfielder@gmail.com>
2020-02-01 13:49:11 -0500

    Align text for clarity

--- playbooks/roles/cfielder/files/.config/nvim/lua/init.lua
+++ playbooks/roles/cfielder/files/.config/nvim/lua/init.lua
@@ -119,21 +119,21 @@ vim.g.signify_sign_change = '~'
 vim.g['sneak#label'] = 1
 
 -- fugitive git bindings
-set_keymap('n', '<leader>ga', ':Git add %:p<CR><CR>', DEFAULT_KEYMAP)
-set_keymap('n', '<leader>gs', ':Gstatus<CR>', DEFAULT_KEYMAP)
-set_keymap('n', '<leader>gc', ':Gcommit -v -q<CR>', DEFAULT_KEYMAP)
-set_keymap('n', '<leader>gt', ':Gcommit -v -q %:p<CR>', DEFAULT_KEYMAP)
-set_keymap('n', '<leader>gd', ':Gdiff<CR>', DEFAULT_KEYMAP)
-set_keymap('n', '<leader>ge', ':Gedit<CR>', DEFAULT_KEYMAP)
-set_keymap('n', '<leader>gr', ':Gread<CR>', DEFAULT_KEYMAP)
-set_keymap('n', '<leader>gw', ':Gwrite<CR><CR>', DEFAULT_KEYMAP)
-set_keymap('n', '<leader>gl', ':silent! Glog<CR>:bot copen<CR>', DEFAULT_KEYMAP)
-set_keymap('n', '<leader>gp', ':Ggrep<Space>', DEFAULT_KEYMAP)
-set_keymap('n', '<leader>gm', ':Gmove<Space>', DEFAULT_KEYMAP)
-set_keymap('n', '<leader>gb', ':Gblame!<CR>', DEFAULT_KEYMAP)
-set_keymap('n', '<leader>go', ':Git checkout<Space>', DEFAULT_KEYMAP)
-set_keymap('n', '<leader>gps', ':Dispatch! git push<CR>', DEFAULT_KEYMAP)
-set_keymap('n', '<leader>gpl', ':Dispatch! git pull<CR>', DEFAULT_KEYMAP)
+set_keymap('n', '<leader>ga',  ':Git add %:p<CR><CR>',            DEFAULT_KEYMAP)
+set_keymap('n', '<leader>gs',  ':Gstatus<CR>',                    DEFAULT_KEYMAP)
+set_keymap('n', '<leader>gc',  ':Gcommit -v -q<CR>',              DEFAULT_KEYMAP)
+set_keymap('n', '<leader>gt',  ':Gcommit -v -q %:p<CR>',          DEFAULT_KEYMAP)
+set_keymap('n', '<leader>gd',  ':Gdiff<CR>',                      DEFAULT_KEYMAP)
+set_keymap('n', '<leader>ge',  ':Gedit<CR>',                      DEFAULT_KEYMAP)
+set_keymap('n', '<leader>gr',  ':Gread<CR>',                      DEFAULT_KEYMAP)
+set_keymap('n', '<leader>gw',  ':Gwrite<CR><CR>',                 DEFAULT_KEYMAP)
+set_keymap('n', '<leader>gl',  ':silent! Glog<CR>:bot copen<CR>', DEFAULT_KEYMAP)
+set_keymap('n', '<leader>gp',  ':Ggrep<Space>',                   DEFAULT_KEYMAP)
+set_keymap('n', '<leader>gm',  ':Gmove<Space>',                   DEFAULT_KEYMAP)
+set_keymap('n', '<leader>gb',  ':Gblame!<CR>',                    DEFAULT_KEYMAP)
+set_keymap('n', '<leader>go',  ':Git checkout<Space>',            DEFAULT_KEYMAP)
+set_keymap('n', '<leader>gps', ':Dispatch! git push<CR>',         DEFAULT_KEYMAP)
+set_keymap('n', '<leader>gpl', ':Dispatch! git pull<CR>',         DEFAULT_KEYMAP)
 
 -- editorconfig
 vim.g.EditorConfig_exclude_patterns = { 'fugitive://.\\*' }
