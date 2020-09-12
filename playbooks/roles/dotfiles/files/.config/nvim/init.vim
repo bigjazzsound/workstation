99ae48befe7ac442318cda16bc52b52de999a0b5
bigjazzsound <craigjamesfielder@gmail.com>
2020-09-12 17:55:46 -0400

    Move treesitter settings to the plugins file

--- playbooks/roles/dotfiles/files/.config/nvim/init.vim
+++ playbooks/roles/dotfiles/files/.config/nvim/init.vim
@@ -8,10 +8,5 @@ EOF
 if has("nvim-0.5.0")
 lua <<EOF
 require('lsp')
-require'nvim-treesitter.configs'.setup {
-  highlight = {
-    enable = true,
-  },
-}
 EOF
 endif
--- playbooks/roles/dotfiles/files/.config/nvim/lua/plugins.lua
+++ playbooks/roles/dotfiles/files/.config/nvim/lua/plugins.lua
@@ -145,3 +145,9 @@ nvimux.bootstrap()
 vim.g.neoterm_autoinsert = 1
 vim.g.neoterm_clear_cmd = { "clear", "" }
 vim.g.neoterm_default_mod = "vertical"
+
+require'nvim-treesitter.configs'.setup {
+  highlight = {
+    enable = true,
+  },
+}
