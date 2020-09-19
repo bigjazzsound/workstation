7a1015fa8bb429237ac55d3d26347378ae3b3296
bigjazzsound <cjf@cjf.ninja>
2020-09-18 23:20:40 -0400

    Add open win function to use with packer for updates.

--- playbooks/roles/dotfiles/files/.config/nvim/lua/plugins.lua
+++ playbooks/roles/dotfiles/files/.config/nvim/lua/plugins.lua
@@ -45,6 +45,24 @@ local fugitive_settings = function()
 end
 
 return require('packer').startup(function()
+local open_win = function()
+  local bufnr = vim.api.nvim_create_buf(false, true)
+  local width = vim.api.nvim_get_option("columns")
+  local height = vim.api.nvim_get_option("lines")
+  local win_height = math.ceil(height * 0.8 - 4)
+  local win_width = math.ceil(width * 0.8)
+  local row = math.ceil((height - win_height) / 2 - 1)
+  local col = math.ceil((width - win_width) / 2)
+  vim.api.nvim_buf_set_option(bufnr, 'bufhidden', 'wipe')
+  return vim.api.nvim_open_win(bufnr, true, {
+    style = "minimal",
+    relative = "win",
+    width = win_width,
+    height = win_height,
+    row = row,
+    col = col
+  })
+end
 
   use {
     'wbthomason/packer.nvim',
