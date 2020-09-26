c0a59ac19c067d89c375f28e6c387469f11efb57
bigjazzsound <craigjamesfielder@gmail.com>
2020-09-25 20:04:24 -0400

    Remove nvimux comments and disable by default

--- playbooks/roles/dotfiles/files/.config/nvim/lua/plugins.lua
+++ playbooks/roles/dotfiles/files/.config/nvim/lua/plugins.lua
@@ -136,12 +136,12 @@ return require('packer').startup({function()
 
       nvimux.config.set_all{
         prefix = '<C-Space>',
-        new_window = 'enew', -- Use 'term' if you want to open a new term for every new window
-        new_tab = nil, -- Defaults to new_window. Set to 'term' if you want a new term for every new tab
+        new_window = 'enew',
+        new_tab = nil,
         new_window_buffer = 'single',
         quickterm_direction = 'botright',
         quickterm_orientation = 'vertical',
-        quickterm_scope = 't', -- Use 'g' for global quickterm
+        quickterm_scope = 't',
         quickterm_size = '80',
       }
 
@@ -154,7 +154,8 @@ return require('packer').startup({function()
       }
 
       nvimux.bootstrap()
-    end
+    end,
+    disable = true, -- I do not use this one often, but I would like to keep the configuration
   }
 
   use {
