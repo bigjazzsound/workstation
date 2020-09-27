cd48154caed785840130ef1c9d0703527e6229b9
bigjazzsound <cjf@cjf.ninja>
2020-09-27 18:18:37 -0400

    Redo lsp maps to use leader

--- playbooks/roles/dotfiles/files/.config/nvim/lua/lsp_config.lua
+++ playbooks/roles/dotfiles/files/.config/nvim/lua/lsp_config.lua
@@ -17,16 +17,13 @@ local on_attach = function(client)
   require('diagnostic').on_attach(client)
   lsp_status.on_attach(client)
 
-  set_keymap('n', 'gd',    ':lua vim.lsp.buf.declaration()<CR>',     DEFAULT_KEYMAP)
-  -- set_keymap('n', '<c-]>', ':lua vim.lsp.buf.definition()<CR>',      DEFAULT_KEYMAP)
-  set_keymap('n', 'K',     ':lua vim.lsp.buf.hover()<CR>',           DEFAULT_KEYMAP)
-  set_keymap('n', 'gD',    ':lua vim.lsp.buf.implementation()<CR>',  DEFAULT_KEYMAP)
-  set_keymap('n', '<c-k>', ':lua vim.lsp.buf.signature_help()<CR>',  DEFAULT_KEYMAP)
-  set_keymap('n', '1gD',   ':lua vim.lsp.buf.type_definition()<CR>', DEFAULT_KEYMAP)
-  set_keymap('n', 'gr',    ':lua vim.lsp.buf.references()<CR>',      DEFAULT_KEYMAP)
-  set_keymap('n', 'g0',    ':lua vim.lsp.buf.document_symbol()<CR>', DEFAULT_KEYMAP)
-  set_keymap('n', 'g[',    '<cmd>PrevDiagnosticCycle<cr>',           DEFAULT_KEYMAP)
-  set_keymap('n', 'g]',    '<cmd>NextDiagnosticCycle<cr>',           DEFAULT_KEYMAP)
+  set_keymap('n', 'K',          ':lua vim.lsp.buf.hover()<CR>',           DEFAULT_KEYMAP)
+  set_keymap('n', '<leader>gd', ':lua vim.lsp.buf.declaration()<CR>',     DEFAULT_KEYMAP)
+  set_keymap('n', '<leader>gD', ':lua vim.lsp.buf.definition()<CR>',      DEFAULT_KEYMAP)
+  set_keymap('n', '<leader>gr', ':lua vim.lsp.buf.references()<CR>',      DEFAULT_KEYMAP)
+  set_keymap('n', '<leader>gt', ':lua vim.lsp.buf.document_symbol()<CR>', DEFAULT_KEYMAP)
+  set_keymap('n', 'g[',         '<cmd>PrevDiagnosticCycle<cr>',           DEFAULT_KEYMAP)
+  set_keymap('n', 'g]',         '<cmd>NextDiagnosticCycle<cr>',           DEFAULT_KEYMAP)
 end
 
 nvim_lsp.bashls.setup{
