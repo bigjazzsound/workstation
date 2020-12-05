bc3e6ec927d77846743af9bbca09fee6797dc395
bigjazzsound <craigjamesfielder@gmail.com>
2020-12-04 19:11:25 -0500

    Disable virtual text on diagnostics

--- playbooks/roles/dotfiles/files/.config/nvim/lua/lsp_config.lua
+++ playbooks/roles/dotfiles/files/.config/nvim/lua/lsp_config.lua
@@ -18,6 +18,7 @@ local on_attach = function(client)
 
   vim.lsp.handlers["textDocument/publishDiagnostics"] = vim.lsp.with(
     vim.lsp.diagnostic.on_publish_diagnostics, {
+      virtual_text = false,
       signs = true,
       update_in_insert = false,
     }
