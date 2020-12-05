809b2ddd617f834948c1421215547d000d73035f
bigjazzsound <craigjamesfielder@gmail.com>
2020-12-04 19:11:11 -0500

    Auto compile plugins on change

--- playbooks/roles/dotfiles/files/.config/nvim/init.lua
+++ playbooks/roles/dotfiles/files/.config/nvim/init.lua
@@ -1,6 +1,7 @@
 require('init')
 require('plugins')
 
+vim.cmd [[ autocmd BufWritePost plugins.lua PackerCompile ]]
 vim.o.statusline = [[ [%n] %f%h%w%m%r %{fugitive#head()} %= %{&paste ?"PASTE ":""}%{&spell?"SPELL ":""} %{&ft} %{luaeval("require('lsp-status').status()")} %l/%L  %P ]]
 vim.o.signcolumn = "yes"
 vim.cmd[[ colorscheme onedark ]]
