bf4580e02d0fceedd7ad476e1107adf62905ad63
bigjazzsound <craigjamesfielder@gmail.com>
2020-12-07 21:14:15 -0500

    Install starship prompt only on Linux operating systems

--- playbooks/roles/dotfiles/files/.config/zsh/zsh.d/50-prompt.zsh
+++ playbooks/roles/dotfiles/files/.config/zsh/zsh.d/50-prompt.zsh
@@ -1,3 +1,5 @@
-[[ -f $(command -v starship) ]] || \
-    $(curl -fsSL https://starship.rs/install.sh | bash -s -- --bin-dir $HOME/.local/bin --yes)
+if [[ "$(uname)" == "Linux" ]] && [[ -f $(command -v starship) ]]; then
+    curl -fsSL https://starship.rs/install.sh | bash -s -- --bin-dir $HOME/.local/bin --yes
+fi
+
 eval "$(starship init zsh)"
