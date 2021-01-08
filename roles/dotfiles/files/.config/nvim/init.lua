6a3b1812964a7145551ae08f8ab3f188b364b45c
bigjazzsound <craigjamesfielder@gmail.com>
2021-01-07 19:34:03 -0500

    Add bubbly.nvim statusline configuration plugin

--- roles/dotfiles/files/.config/nvim/init.lua
+++ roles/dotfiles/files/.config/nvim/init.lua
@@ -2,6 +2,5 @@ require('init')
 require('plugins')
 
 vim.cmd [[ autocmd BufWritePost plugins.lua PackerCompile ]]
-vim.o.statusline = [[ [%n] %f%h%w%m%r %{fugitive#head()} %= %{&paste ?"PASTE ":""}%{&spell?"SPELL ":""} %{&ft} %{luaeval("require('lsp-status').status()")} %l/%L  %P ]]
 vim.o.signcolumn = "yes"
 vim.cmd[[ colorscheme onedark ]]
--- roles/dotfiles/files/.config/nvim/lua/plugins.lua
+++ roles/dotfiles/files/.config/nvim/lua/plugins.lua
@@ -155,6 +155,39 @@ return require('packer').startup({function()
   }
 
   use {
+    'datwaft/bubbly.nvim', config = function()
+      vim.g.bubbly_palette = {
+        background = "#34343c",
+        foreground = "#c5cdd9",
+        black      = "#3e4249",
+        red        = "#ec7279",
+        green      = "#a0c980",
+        yellow     = "#deb974",
+        blue       = "#6cb6eb",
+        purple     = "#d38aea",
+        cyan       = "#5dbbc1",
+        white      = "#c5cdd9",
+        lightgrey  = "#57595e",
+        darkgrey   = "#404247",
+      }
+      vim.g.bubbly_statusline = {
+        'mode',
+        'truncate',
+        'path',
+        'branch',
+        'signify',
+        'divisor',
+        'filetype',
+        {
+          {
+            data = '%l/%L',
+          }
+        },
+      }
+    end
+  }
+
+  use {
     'junegunn/goyo.vim',
     cmd = {'Goyo'},
   }
