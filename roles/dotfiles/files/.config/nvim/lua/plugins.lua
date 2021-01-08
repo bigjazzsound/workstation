29454dc527c42b63a2c85d8fedc5921e4bf2dc00
bigjazzsound <craigjamesfielder@gmail.com>
2021-01-07 19:34:28 -0500

    Automatically install maintained treesitter languages

--- roles/dotfiles/files/.config/nvim/lua/plugins.lua
+++ roles/dotfiles/files/.config/nvim/lua/plugins.lua
@@ -256,6 +256,7 @@ return require('packer').startup({function()
     'nvim-treesitter/nvim-treesitter',
     config = function()
       require('nvim-treesitter.configs').setup {
+        ensure_installed = "maintained",
         highlight = {
           enable = true,
         },
