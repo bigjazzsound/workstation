20233d4c25d693acf6162b196440cac4bfc8d919
bigjazzsound <craigjamesfielder@gmail.com>
2021-01-25 21:45:39 -0500

    Add maps for neoterm plugin

--- roles/dotfiles/files/.config/nvim/lua/plugins.lua
+++ roles/dotfiles/files/.config/nvim/lua/plugins.lua
@@ -187,6 +187,11 @@ return require('packer').startup({function()
       vim.g.neoterm_autoinsert = 1
       vim.g.neoterm_clear_cmd = { "clear", "" }
       vim.g.neoterm_default_mod = "vertical"
+      vim.api.nvim_set_keymap('n', '<leader>tT', '<CMD>TtoggleAll<CR>',    DEFAULT_KEYMAP)
+      vim.api.nvim_set_keymap('n', '<leader>tc', '<CMD>Tclear!<CR>',       DEFAULT_KEYMAP)
+      vim.api.nvim_set_keymap('n', '<leader>tF', '<CMD>TREPLSendFile<CR>', DEFAULT_KEYMAP)
+      vim.api.nvim_set_keymap('n', '<leader>tn', '<CMD>Tnew<CR>',          DEFAULT_KEYMAP)
+      vim.api.nvim_set_keymap('n', '<leader>tt', '<CMD>Ttoggle<CR>',       DEFAULT_KEYMAP)
     end
   }
 
