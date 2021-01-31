046ec45fef91a60883643ea0bb58780e607dd517
Craig James Fielder <cjf@cjf.ninja>
2021-01-31 16:04:05 -0500

    Test out vim-compe for completion

--- roles/dotfiles/files/.config/nvim/lua/plugins.lua
+++ roles/dotfiles/files/.config/nvim/lua/plugins.lua
@@ -211,6 +211,37 @@ return require('packer').startup({function()
   }
 
   use {
+    'hrsh7th/nvim-compe',
+    config = function()
+      require('compe').setup {
+        enabled = true,
+        autocomplete = true,
+        debug = false,
+        min_length = 1,
+        preselect = 'enable',
+        throttle_time = 80,
+        source_timeout = 200,
+        incomplete_delay = 400,
+        allow_prefix_unmatch = false,
+
+        source = {
+          path = true,
+          buffer = true,
+          calc = true,
+          vsnip = true,
+          nvim_lsp = true,
+          nvim_lua = true,
+          spell = true,
+          snippets_nvim = true,
+        },
+      }
+      vim.api.nvim_set_keymap('i', '<C-n>', 'compe#complete()',      { silent = true, expr = true, noremap = true })
+      vim.api.nvim_set_keymap('i', '<CR>',  'compe#confirm("<CR>")', { silent = true, expr = true, noremap = true })
+      vim.api.nvim_set_keymap('i', '<CR>',  'compe#close("<C-e>")',  { silent = true, expr = true, noremap = true })
+    end,
+  }
+
+  use {
     'nvim-treesitter/nvim-treesitter',
     config = function()
       require('nvim-treesitter.configs').setup {
