702b194d87c0f4d4ce78eb060dbf358d78ee923f
Craig James Fielder <cjf@cjf.ninja>
2021-03-06 22:51:22 -0500

    Add keymaps and incremental selection to treesitter settings

--- roles/dotfiles/files/.config/nvim/lua/plugins.lua
+++ roles/dotfiles/files/.config/nvim/lua/plugins.lua
@@ -175,6 +175,15 @@ return require('packer').startup({function()
         highlight = {
           enable = true,
         },
+        incremental_selection = {
+          enable = true,
+          keymaps = {
+            init_selection = "gnn",
+            node_incremental = "grn",
+            scope_incremental = "grc",
+            node_decremental = "grm",
+          },
+        },
       }
     end
   }
