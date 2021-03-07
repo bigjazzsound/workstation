e4bae4e7965698ca415699fea417f393080fa3d2
Craig James Fielder <cjf@cjf.ninja>
2021-03-06 22:47:36 -0500

    Remove previewer overrides since they are now the default

--- roles/dotfiles/files/.config/nvim/lua/bigjazzsound/telescope/init.lua
+++ roles/dotfiles/files/.config/nvim/lua/bigjazzsound/telescope/init.lua
@@ -26,10 +26,6 @@ require('telescope').setup{
     width = 0.95,
     winblend = 5,
     preview_cutoff = 120,
-    -- These previewers are experimental but they are FAST
-    file_previewer = previewers.vim_buffer_cat.new,
-    grep_previewer = previewers.vim_buffer_vimgrep.new,
-    qflist_previewer = previewers.vim_buffer_qflist.new,
   }
 }
 
