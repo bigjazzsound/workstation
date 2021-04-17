a33b102adfbb530125aeee65f40a5a07703ecf0a
Craig James Fielder <cjf@cjf.ninja>
2021-04-16 20:47:32 -0400

    Move git settings to separate file

---
+++ roles/dotfiles/files/.config/nvim/lua/bigjazzsound/git.lua
@@ -0,0 +1,5 @@
+  require('neogit').setup()
+  vim.api.nvim_set_keymap('n', '<leader>gs', '<CMD>lua require("neogit").status.create("vsplit")<CR>', DEFAULT_KEYMAP)
+  vim.cmd [[highlight NeogitDiffAddHighlight guifg=#98c379 guibg=#3e4452]]
+  vim.cmd [[highlight NeogitDiffDeleteHighlight guifg=#e06c75 guibg=#3e4452]]
+  vim.cmd [[highlight NeogitDiffContextHighlight guibg=#282C34]]
--- roles/dotfiles/files/.config/nvim/lua/plugins.lua
+++ roles/dotfiles/files/.config/nvim/lua/plugins.lua
@@ -210,13 +210,7 @@ return require('packer').startup({function(use)
 
   use {
     'TimUntersberger/neogit',
-    config = function()
-      require('neogit').setup()
-      vim.api.nvim_set_keymap('n', '<leader>gs', '<CMD>lua require("neogit").status.create("vsplit")<CR>', DEFAULT_KEYMAP)
-      vim.cmd [[highlight NeogitDiffAddHighlight guifg=#98c379 guibg=#3e4452]]
-      vim.cmd [[highlight NeogitDiffDeleteHighlight guifg=#e06c75 guibg=#3e4452]]
-      vim.cmd [[highlight NeogitDiffContextHighlight guibg=#282C34 ]]
-    end,
+    config = function() require('bigjazzsound.git') end,
   }
 
   -- colors
