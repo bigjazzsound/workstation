3c2d3b82f944b4599656eb6d357ee2cb8ee53c7f
Craig James Fielder <cjf@cjf.ninja>
2021-04-16 21:12:19 -0400

    Add command to query Spotify and return the results in telescope

--- roles/dotfiles/files/.config/nvim/lua/bigjazzsound/commands.lua
+++ roles/dotfiles/files/.config/nvim/lua/bigjazzsound/commands.lua
@@ -74,4 +74,126 @@ M.open_win = function(text)
   end, 3000)
 end
 
+-- TODO - figure out how to consistently get authenticated with the Spotify API
+-- https://accounts.spotify.com/en/authorize?client_id=d94e23075223431db646be5712b49f46&redirect_uri=http:%2F%2Fcraigjamesfielder.com%2F&response_type=code&scope=user-read-currently-playing
+-- curl -d client_id="${SPOTIFY_CLIENT_ID}" -d client_secret="${SPOTIFY_CLIENT_SECRET}" -d grant_type=authorization_code -d code="${SPOTIFY_CODE}" -d redirect_uri="http://craigjamesfielder.com/" https://accounts.spotify.com/api/token
+-- .access_token
+
+local get_spotify_token = function()
+  local client_id = os.getenv("SPOTIFY_CLIENT_ID")
+  local client_secret = os.getenv("SPOTIFY_CLIENT_SECRET")
+  local output = require('plenary.job'):new {
+    command = 'curl',
+    args = {
+      '-s', '-X', 'POST',
+      '-d', 'client_id='..client_id,
+      '-d', 'client_secret='..client_secret,
+      '-d', 'grant_type=client_credentials',
+      "https://accounts.spotify.com/api/token"
+    },
+  }:sync()
+
+  local json = vim.fn.json_decode(output)
+
+  return json.access_token
+end
+
+M.query_spotify = function()
+  -- TODO - error handling
+  -- Do a primary request to get what is playing
+  local token = os.getenv("SPOTIFY_ACCESS_TOKEN")
+  local current_track = require('plenary.job'):new {
+    command = 'curl',
+    args = {
+      '-s', '-X', 'GET',
+      '-H', 'Content-Type: application/json',
+      '-H', 'Authorization: Bearer '..token,
+      "https://api.spotify.com/v1/me/player/currently-playing?market=US"
+    },
+  }:sync()
+
+  local track_context = vim.fn.json_decode(current_track).context.href
+
+  -- Do a secondary request to get the album or playlist of the current track
+  local output = require('plenary.job'):new {
+    command = 'curl',
+    args = {
+      '-s', '-X', 'GET',
+      '-H', 'Content-Type: application/json',
+      '-H', 'Authorization: Bearer '..token,
+      track_context.."?market=US"
+    },
+  }:sync()
+
+  local json = vim.fn.json_decode(output)
+  -- P(json)
+
+  local source = function(source)
+    local tracks = {}
+    if source.type == "playlist" then
+      for _, track in ipairs(source.tracks.items) do
+        table.insert(tracks, {
+          name = track.track.name,
+          artist = track.track.artists[1].name,
+          duration = {
+            minutes = math.floor(track.track.duration_ms / 60000),
+            seconds = math.floor(track.track.duration_ms / 1000 % 60),
+          },
+          album = track.track.album.name,
+        })
+      end
+      else
+      string.format("%s - %s", json.artists[1].name, json.name)
+      for _, track in ipairs(source.tracks.items) do
+        table.insert(tracks, {
+          name = track.track.name,
+          duration = {
+            minutes = math.floor(track.track.duration_ms / 60000),
+            seconds = math.floor(track.track.duration_ms / 1000 % 60),
+          },
+          album = track.track.album.name,
+        })
+      end
+    end
+    return tracks
+  end
+
+  local current = source(json)
+
+  -- TODO make this play the selected track
+  local foo = function(prompt_bufnr)
+    actions.close(prompt_bufnr)
+    local entry = action_state.get_selected_entry()
+    -- TODO - play the track
+    print(entry.value)
+  end
+
+  -- P(source(json))
+
+  -- TODO - this handles albums. I'm not sure how if it will work with a playlist
+  -- TODO - split entry by album, track length, etc. Ex. https://github.com/nvim-telescope/telescope.nvim/pull/754/files
+  pickers:new {
+    results_title = "Tracks",
+    finder = finders.new_table {
+      results = current,
+      entry_maker = function(entry)
+        -- local display = string.format("[%02d] %s [%02d:%02d]", entry.track_number, entry.name, minutes, seconds)
+        local display = string.format("%s [%02d:%02d]", entry.name, entry.duration.minutes, entry.duration.seconds)
+        return {
+          display = display,
+          value = display,
+          ordinal = display,
+          href = entry.href,
+        }
+      end
+    },
+    sorter = sorters.get_generic_fuzzy_sorter(),
+    attach_mappings = function(_, map)
+      map('i', '<CR>', foo)
+      map('n', '<CR>', foo)
+      return true
+    end
+  }:find()
+end
+
 return M
