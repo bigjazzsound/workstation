5487410347c2c2bddb3bd541a2c12dbf7f7472ed
bigjazzsound <craigjamesfielder@gmail.com>
2021-04-23 23:46:31 -0400

    Update zsh plugins

--- roles/dotfiles/files/.config/zsh/zsh.d/60-plugins.zsh
+++ roles/dotfiles/files/.config/zsh/zsh.d/60-plugins.zsh
@@ -2,50 +2,36 @@ ZPLUGINDIR=$HOME/.zsh
 if [[ ! -d  $ZPLUGINDIR ]]; then
     mkdir $ZPLUGINDIR
     [[ -f $(command -v git) ]] && \
-        git clone https://github.com/marlonrichert/zsh-snap.git $ZPLUGINDIR/zsh-snap
+        git clone --depth 1 -- https://github.com/marlonrichert/zsh-snap.git $ZPLUGINDIR/zsh-snap
 fi
+
+setopt histfcntllock histignorealldups histsavenodups sharehistory
 source $ZPLUGINDIR/zsh-snap/znap.zsh
 
-PLUGINS=(
-    "Aloxaf/fzf-tab"
-    "DarrinTisdale/zsh-aliases-exa"
-    "MichaelAquilina/zsh-auto-notify"
-    "blimmer/zsh-aws-vault"
-    "ohmyzsh/ohmyzsh"
-    "zdharma/fast-syntax-highlighting"
-    "zpm-zsh/clipboard"
-    "zsh-users/zsh-autosuggestions"
-    "zsh-users/zsh-completions"
-)
+znap source marlonrichert/zcolors
+znap eval zcolors "zcolors ${(q)LS_COLORS}"
 
-for PLUGIN in ${PLUGINS}; do
-    if [[ ! -d ${ZPLUGINDIR}/$(basename ${PLUGIN}) ]]; then
-        ZPLUGINS+=($(echo "https://github.com/${PLUGIN}.git"))
-    fi
-done; [[ -n ${ZPLUGINS} ]] && znap clone $(echo ${ZPLUGINS} | xargs)
-
-OMZ="ohmyzsh"
-znap source ${OMZ}
-znap source ${OMZ} \
-    plugins/ansible \
-    plugins/fzf
+# znap source Aloxaf/fzf-tab
+znap source ohmyzsh/ohmyzsh plugins/fzf
 FZF_DEFAULT_OPTS='--height=60% --layout=reverse --multi'
 if [[ -f $(command -v fd) ]]; then
     FZF_DEFAULT_COMMAND='fd --type f --hidden --follow --exclude .git venv'
     FZF_CTRL_T_COMMAND='fd --type f --follow --exclude .git'
     FZF_CTRL_T_OPTS='--multi --preview="bat --line-range :50 --color=always --style plain {}"'
 fi
-znap source zsh-aws-vault
-znap source fzf-tab
-znap source ${OMZ} "lib/history.zsh"
-znap source fast-syntax-highlighting
-znap source zsh-completions
-znap source zsh-autosuggestions
-znap source clipboard
-znap source zsh-aliases-exa
-bindkey '^n' autosuggest-accept
-export ZSH_AUTOSUGGEST_HIGHLIGHT_STYLE="fg=14,bold,underline"
 
-# Prevent OMZ from auto-updating
-DISABLE_UPDATE_PROMPT=true
-DISABLE_AUTO_UPDATE=true
+znap source zsh-users/zsh-autosuggestions
+ZSH_AUTOSUGGEST_HIGHLIGHT_STYLE="fg=14,bold,underline"
+bindkey '^n' autosuggest-accept
+
+# TODO - not sure if I wan to use this plugin yet or not
+# zstyle ':autocomplete:tab:*' widget-style menu-select
+# zstyle ':autocomplete:tab:*' fzf-completion yes
+# znap source marlonrichert/zsh-autocomplete
+# zstyle ':autocomplete:*' default-context ''
+# znap source zsh-users/zsh-completions
+
+znap source DarrinTisdale/zsh-aliases-exa
+znap source blimmer/zsh-aws-vault
+
+znap source MichaelAquilina/zsh-auto-notify
