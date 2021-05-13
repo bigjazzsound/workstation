fe9a26b254f90075096fe55069d9d30ec0862570
Craig James Fielder <cjf@cjf.ninja>
2021-05-13 11:44:42 -0400

    Update pip and wheel in one step

--- bootstrap.sh
+++ bootstrap.sh
@@ -4,8 +4,7 @@ if [[ ! -d venv ]]; then
     python3 -m venv venv
 fi
 
-venv/bin/pip install -U pip
-venv/bin/pip install wheel
+venv/bin/pip install -U pip wheel
 venv/bin/pip install ansible-base jmespath
 
 venv/bin/ansible-galaxy install -r requirements.yml
