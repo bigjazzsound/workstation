abb4aa62370851508c3e2dd0684dacbd14768a87
bigjazzsound <craigjamesfielder@gmail.com>
2021-06-22 23:27:08 -0400

    Add lsp scanning progress to status bar

--- roles/dotfiles/files/.config/nvim/lua/bigjazzsound/status.lua
+++ roles/dotfiles/files/.config/nvim/lua/bigjazzsound/status.lua
@@ -1,3 +1,16 @@
+local lsp_progress = function()
+  local messages = vim.lsp.util.get_progress_messages()
+  if #messages == 0 then return end
+  local status = {}
+  for _, msg in pairs(messages) do
+    table.insert(status, (msg.percentage or 0) .. "%% " .. (msg.title or ""))
+  end
+  local spinners = { "⠋", "⠙", "⠹", "⠸", "⠼", "⠴", "⠦", "⠧", "⠇", "⠏" }
+  local ms = vim.loop.hrtime() / 1000000
+  local frame = math.floor(ms / 120) % #spinners
+  return table.concat(status, " | ") .. " " .. spinners[frame + 1]
+end
+
 require "lualine".setup {
   options = {
     theme = "tokyonight",
@@ -21,7 +34,7 @@ require "lualine".setup {
       "fileformat",
       { "filetype", colored = false },
     },
-    lualine_y = { "progress" },
+    lualine_y = {{lsp_progress}},
     lualine_z = { "location" },
   },
   inactive_sections = {
