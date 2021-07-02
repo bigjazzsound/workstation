5941a8c7d5b48b870bfc23d8664fcf93f3b0cd08
bigjazzsound <craigjamesfielder@gmail.com>
2021-07-01 22:26:25 -0400

    Switch from ansible-base to ansible-core

--- bootstrap.sh
+++ bootstrap.sh
@@ -3,6 +3,6 @@
 [[ ! -d venv ]] && python3 -m venv venv
 
 venv/bin/pip install -U pip wheel
-venv/bin/pip install ansible-base jmespath
+venv/bin/pip install ansible-core jmespath
 
 venv/bin/ansible-galaxy install -r requirements.yml
